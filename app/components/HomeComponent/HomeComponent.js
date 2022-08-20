c94e9b9adf1f33a8587e35c02b86c45a1226f250
Facundo Corigliano <facundo.corigliano@mercadolibre.com>
2022-08-20 00:54:20 -0300

    reordenamiento

--- app/components/HomeComponent/HomeComponent.js
+++ app/components/HomeComponent/HomeComponent.js
@@ -38,8 +38,8 @@ const HomeComponent = ({ i18n }) => {
           <li><a href="#">El que más copas ganó</a></li>
           <li><a href="#">El que llena en todos lados</a></li>
           <li><a href="#">De La Boca hasta Japón</a></li>
-          <li id="rightButton" ><a href="#" onClick={changePositionRight}>Mover a la derecha</a></li>
           <li id="leftButton"><a href="#" onClick={changePositionLeft}>Mover a la izquierda</a></li>
+          <li id="rightButton" ><a href="#" onClick={changePositionRight}>Mover a la derecha</a></li>
           <li id="bottomButton" ><a href="#" onClick={changeHorizontalPosition}>Mover abajo</a></li>
         </ul>
       </nav>
